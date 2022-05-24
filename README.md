0a599a64be07726adfcce97224dd154f8e19e741
nada <nada.mohamed@extech.com>
2022-05-24 19:45:36 +0200

    login user, or create a new one

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1. Trying Alpine.js: show/hide some text on click
 
+2. Login a user, or create it using firstOrCreate
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,8 +13,16 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register-user', [UserController::class, 'register']);
+
+Route::post('/login-user', [UserController::class, 'login'])->name('login-user');
+
+// login or create
+Route::post('/login-create-user', [UserController::class, 'loginOrCreate'])
+    ->name('login-create-user');
