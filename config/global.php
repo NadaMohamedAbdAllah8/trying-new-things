e702e01257f16ac756b4607fcb27e1b4cef93b8c
nada <nada.mohamed@extech.com>
2022-05-30 15:11:04 +0200

    adding global file for constants

---
+++ config/global.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'defaultPagination' => 5,
+    'paginationFirstIncrease' => 10,
+    'paginationSecondIncrease' => 15,
+    'paginationThirdIncrease' => 20,
+    'defaultTitle'=>'Nada is trying new things!'
+]
+
+?>
